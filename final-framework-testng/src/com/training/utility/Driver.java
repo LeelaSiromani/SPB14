fda84d6a29eef32ebd6484530bd554467ea2c75f
LEELASIROMANI <LEELASIROMANI@lbandaru.in.ibm.com>
2020-03-13 10:17:02 +0530

    3 Tests- TeacherLogin, StudentLogin & Teacher Registration

--- final-framework-testng/src/com/training/utility/Driver.java
+++ final-framework-testng/src/com/training/utility/Driver.java
@@ -3,13 +3,13 @@ package com.training.utility;
 public interface Driver {
 	// KEYS 
 	String CHROME ="webdriver.chrome.driver"; 
-	String FIREFOX ="webdriver.firefox.marionette"; 
+	String FIREFOX ="webdriver.gecko.driver"; 
 	String IE ="webdriver.ie.driver";
 	String PHANTOM="phantomjs.binary.path";
 	
 	// PATH 
 	String CHROME_PATH="C:\\Users\\LEELASIROMANI\\Documents\\ATUReporter_Selenium_testNG_5.5 BETA + ATU Recorder 2.1+javadoc+propfile\\ATUReporter_Selenium_testNG_5.5 BETA + ATU Recorder 2.1+javadoc+propfile\\chromedriver.exe";
-	String FIREFOX_PATH ="";
+	String FIREFOX_PATH ="C:\\Selenium3\\geckodriver.exe";
 	String IE_PATH ="";
 	String PHANTOM_PATH="";
 	
